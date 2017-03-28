da7d936dbfbdb65ecc004cf884c030e8e48a5b50
DSobscure <snbruce13@yahoo.com.tw>
2017-03-28 22:06:49 +0800

    final

--- NIN_Cifar10.py
+++ NIN_Cifar10.py
@@ -17,8 +17,10 @@ def main(argv = None):
     learningRate = TensorFlow.placeholder(TensorFlow.float32)
 
     crossEntropy = TensorFlow.reduce_mean(TensorFlow.nn.softmax_cross_entropy_with_logits(labels=output, logits=network))
+    l2_loss = TensorFlow.add_n([TensorFlow.nn.l2_loss(v) for v in TensorFlow.trainable_variables()])
+    cost = crossEntropy + 0.0001 * l2_loss
 
-    trainStep = TensorFlow.train.MomentumOptimizer(learningRate, 0.9).minimize(crossEntropy)
+    trainStep = TensorFlow.train.MomentumOptimizer(learningRate, 0.9, use_nesterov = True).minimize(cost)
 
     correct_prediction = TensorFlow.equal(TensorFlow.argmax(network,1), TensorFlow.argmax(output,1))
     accuracy = TensorFlow.reduce_mean(TensorFlow.cast(correct_prediction, TensorFlow.float32))
@@ -27,6 +29,8 @@ def main(argv = None):
     session.run(TensorFlow.global_variables_initializer())
     saver = TensorFlow.train.Saver()
     lossSum = 0.
+    #saver.restore(session, "../Models/model.ckpt")
+    #print("Model restored.")
     
     def Testing(sess):
         acc = 0.0
@@ -52,7 +56,8 @@ def main(argv = None):
     savePath = saver.save(session, "../Models/model.ckpt")
     print("Model saved in file: %s" % savePath)
 
-    for i in range(1,32001): #81~160 epochs
+
+    for i in range(1,16001): #81~160 epochs
         imageBatch, labelBatch = Cifar10Manager.TakeRandomTranningSampleBatch(tranningSet, 128)
         ## test every 100 step
         if i%400 == 0:
@@ -62,13 +67,13 @@ def main(argv = None):
                 file.writelines(str(acc) + "\t" + str(lossSum/400) + "\n")
             lossSum = 0.
         ## trianing
-        [_, loss] = session.run([trainStep, crossEntropy],feed_dict={x: imageBatch, output: labelBatch, keepProbe: 0.5, learningRate: 0.005})  #learning rate 0.05
+        [_, loss] = session.run([trainStep, crossEntropy],feed_dict={x: imageBatch, output: labelBatch, keepProbe: 0.5, learningRate: 0.005})  #learning rate 0.005
         lossSum = lossSum + loss
     
     savePath = saver.save(session, "../Models/model.ckpt")
     print("Model saved in file: %s" % savePath)
 
-    for i in range(1,16001): #161~200 epochs
+    for i in range(1,8001): #161~200 epochs
         imageBatch, labelBatch = Cifar10Manager.TakeRandomTranningSampleBatch(tranningSet, 128)
         ## test every 100 step
         if i%400 == 0:
